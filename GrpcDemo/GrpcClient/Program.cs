5af1a3e34b7c2a699992a94c5358e0f555683e72
Daniel John <danieljohn371@gmail.com>
2025-01-14 14:16:36 -0500

    Grpc client

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class GrpcClientClass
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "test" };
+            var channel = GrpcChannel.ForAddress("https://localhost:7062");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.Read();
+        }
+    }
+}
